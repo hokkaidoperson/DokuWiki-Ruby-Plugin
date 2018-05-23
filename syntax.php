0ac59e08d72d319f47ba90966d73060b3aafd89f
Satoshi Sahara <sahara.satoshi@gmail.com>
2018-05-23 20:58:08 +0900

    omit the PHP closing tag at the end of the file
    This prevents accidental whitespace or new lines being added after the
    PHP closing tag, which may cause unwanted effects because PHP will start
    output buffering when there is no intention from the programmer to send
    any output at that point in the script.
    see http://php.net/manual/en/language.basic-syntax.phptags.php

--- syntax.php
+++ syntax.php
@@ -3,13 +3,13 @@
 if(!defined('DOKU_INC')) define('DOKU_INC',realpath(dirname(__FILE__).'/../../').'/');
 if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 require_once(DOKU_PLUGIN.'syntax.php');
- 
+
 class syntax_plugin_ruby extends DokuWiki_Syntax_Plugin {
- 
+
     function getType(){
         return 'substition';
     }
- 
+
     function getSort(){
         return 150;
     }
@@ -17,14 +17,12 @@ class syntax_plugin_ruby extends DokuWiki_Syntax_Plugin {
     function connectTo($mode) {
       $this->Lexer->addSpecialPattern('\{\{ruby\|[^}]*\}\}',$mode,'plugin_ruby');
     }
-  
+
     function handle($match, $state, $pos, Doku_Handler $handler) {
         return explode('|', substr($match, strlen('{{ruby|'), -2));
     }
- 
+
     function render($format, Doku_Renderer $renderer, $data) {
         $renderer->doc .= '<ruby><rb>' .htmlspecialchars($data[0]) . '</rb><rp>' . $this->getConf('parenthesis') . '</rp><rt>' .htmlspecialchars($data[1]) . '</rt><rp>' . $this->getConf('parenthesisClosing') . '</rp></ruby>';
     }
 }
- 
-?>
