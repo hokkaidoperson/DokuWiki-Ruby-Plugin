1185fce3325869493273c22e1e3d38033421cfb2
Satoshi Sahara <sahara.satoshi@gmail.com>
2018-05-23 21:23:02 +0900

    remove superfluous code

--- syntax.php
+++ syntax.php
@@ -1,8 +1,6 @@
 <?php
- 
-if(!defined('DOKU_INC')) define('DOKU_INC',realpath(dirname(__FILE__).'/../../').'/');
-if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-require_once(DOKU_PLUGIN.'syntax.php');
+// must be run within Dokuwiki
+if(!defined('DOKU_INC')) die();
 
 class syntax_plugin_ruby extends DokuWiki_Syntax_Plugin {
 
