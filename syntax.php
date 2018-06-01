b7d54f9cdfa6569ee204f2f78a64df41bd0d9255
Satoshi Sahara <sahara.satoshi@gmail.com>
2018-06-01 19:00:26 +0900

    refactor render method
    set static ruby parenthesis (rp), and make different output for non-xhtml format mode.

--- syntax.php
+++ syntax.php
@@ -58,6 +58,35 @@ class syntax_plugin_ruby extends DokuWiki_Syntax_Plugin {
      * Create output
      */
     function render($format, Doku_Renderer $renderer, $data) {
+        static $rp;
+        if (!isset($rp)) {
+            if (utf8_strlen($this->getConf('parentheses')) > 1) {
+                // get a pair of ruby parentheses
+                $rp[0] = utf8_substr($this->getConf('parentheses'), 0, 1);
+                $rp[1] = utf8_substr($this->getConf('parentheses'), 1, 1);
+            } else {
+                // set an empty array
+                $rp = array();
+            }
+        }
+
+        if ($format == 'xhtml') {
+            // create a ruby annotation for words and phrases
+            $renderer->doc .= '<ruby>';
+            $renderer->doc .= '<rb>'.hsc($data[0]).'</rb>';
+            $renderer->doc .= isset($rp[0]) ? '<rp>'.hsc($rp[0]).'</rp>' : '';
+            $renderer->doc .= '<rt>'.hsc($data[1]).'</rt>';
+            $renderer->doc .= isset($rp[1]) ? '<rp>'.hsc($rp[1]).'</rp>' : '';
+            $renderer->doc .= '</ruby>';
+        } else {
+            // For non-XHTML format mode, render base text
+            // omit ruby text if a pair of parentheses is not set
+            $renderer->doc .= hsc($data[0]);
+            $renderer->doc .= !empty($rp) ? hsc($rp[0].$data[1].$rp[1]) : '';
+        }
+        return;
+
+
         if ($data[2] == null) {
             $renderer->doc .= '<ruby>';
             $renderer->doc .= '<rb>' . htmlspecialchars($data[0]) . '</rb>';
