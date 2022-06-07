b359855ecddb8e92abc083435382423dce60c323
Schplurtz le Déboulonné <schplurtz@laposte.net>
2022-06-07 20:30:16 +0200

    translation update

---
+++ lang/fr/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
+ */
+$lang['parentheses']           = 'Parenthèses concernant ruby (utilisées pour le mode de rendu "métadonnées" (par exemple pour le rendu d\'un flux RSS) et pour les périphériques ne comprenant pas la syntaxe ruby)<br>
+Le premier caractère du texte dans ce cadre de texte sera une parenthèse et le second, un caractère correct. Les caractères suivants seront ignorés. S\'il n\'y a qu\'un seul caractère ou si le texte est vide, il n\'y aura pas de parenthèse.';
